0cff1c215cd68bd221c55f4c9e351b9643f5c384
Shane Jeremich <shane.jeremich@gmail.com>
2020-08-09 06:13:37 -0600

    2 of 3 done

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -2,7 +2,30 @@ def intersection(arrays):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    result = []
+
+    # sort the arrays by shortest to longest
+    # call the sorted() function --> returns values in ascending order
+    arrays = sorted(arrays, key=len)
+    # convert each array to dicts and store them in a new list
+    dictionaries = []
+    # loop through the arrays of arrays
+    for arr in arrays:
+        # convert each of them into dict (dictionary comprehension)
+        arr_dictionary = {i: True for i in arr}
+        # add that dict to the dictionaries list
+        dictionaries.append(arr_dictionary)
+    # loop through the values of the first SHORTEST array
+    for i in arrays[0]:
+        # starts as true
+        values_in_all = True
+        # loop (again) through the dictionaries
+        for d in dictionaries:
+            # d is a dictionary representing one of the lists
+            if i not in d:
+                values_in_all = False
+        if values_in_all:
+            result.append(i)
 
     return result
 
